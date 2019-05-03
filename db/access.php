57011aa9b39c5b0634f4075aa0e97bb44f6e7135
pc-magas <katanophoros@gmail.com>
2019-05-03 17:56:02 +0300

    Remove Spaces

--- db/access.php
+++ db/access.php
@@ -12,14 +12,12 @@
     ),
     'block/userlist:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
- 
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         ),
- 
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ),
     
