f5ea08805d9c44fed75fec4930c1a7143bd6e535
pc-magas <katanophoros@gmail.com>
2019-05-06 00:40:07 +0300

    Keep translations

--- lang/el/block_userlist.php
+++ lang/el/block_userlist.php
@@ -1,3 +1,7 @@
 <?php
 $string['pluginname']='Στατιστικά Εισόδου-Εξόδου Χρηστών';
 $string['userlist'] = $string['pluginname'];
+$string['username'] = 'Όνομα Χρήστη';
+$string['loginTime'] = 'Χρόνος Εισόδου';
+$string['logoutTime'] = 'Χρόνος Εξόδου';
+$string['sessionDuration'] = 'Διάρκεια'; 
--- lang/en/block_userlist.php
+++ lang/en/block_userlist.php
@@ -1,3 +1,7 @@
 <?php
 $string['pluginname']='User list and Statistics';
 $string['userlist'] = $string['pluginname'];
+$string['username'] = 'Username';
+$string['loginTime'] = 'Time User logged in';
+$string['logoutTime'] = 'Time User logged out';
+$string['sessionDuration'] = 'Session Duration'; 
