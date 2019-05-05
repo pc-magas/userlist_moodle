a72034eab99bce92247e47e9f1fe0bc4e90b0d70
pc-magas <katanophoros@gmail.com>
2019-05-06 00:52:54 +0300

    Fetch Values

--- block_userlist.php
+++ block_userlist.php
@@ -1,6 +1,12 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
+// Supressing warning class
+class block_userlist_context {
+  public $text='';
+  public $foote='';
+}
+
 class block_userlist extends block_base {
     public function init() {
         $this->title = get_string('userlist', 'block_userlist');
@@ -15,13 +21,31 @@ class block_userlist extends block_base {
         //   return $this->content;
         // }
         
-        $user = $DB->get_record_sql('SELECT COUNT(*) as total_users FROM {user};');
 
-        $this->content         =  new stdClass;
-        $this->content->text  .= 'The content of our ';
-        $this->content->text  .= html_writer::tag('span','UserList',['style'=>'color:red']);
-        $this->content->text  .= ' block!';
-        $this->content->footer = "Τotal Users: $user->total_users";
+        $query='SELECT username,login_time,logout_time,session_duration FROM {block_userlist} ORDER BY login_time DESC;';
+        $results = $DB->get_records_sql($query);
+
+        $this->content =  new block_userlist_context();
+        
+        $table = new html_table();
+        $table->head = [
+            get_string('username', 'block_userlist'),
+            get_string('loginTime', 'block_userlist'),
+            get_string('logoutTime', 'block_userlist'),
+            get_string('sessionDuration', 'block_userlist'),
+        ];
+
+        foreach ($results as $result) {
+            $table->data[] = [
+                $result->username,
+                date('D d/m/Y H:i:s', $result->login_time),
+                date('D d/m/Y H:i:s', $result->logout_time),
+                gmdate('H:i:s', $result->session_duration),
+            ];
+        }
+
+        $this->content->text=html_writer::table($table);
+
         return $this->content;
     }
 }
